d158cf4374982caff047e93c36723ba945d0cd7f
Amelia Septiani <amelianew7780@gmail.com>
2026-07-07 10:45:59 +0700

    AndroidManifest.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,8 @@ android {
 
 dependencies {
     dependencies {
+        // ... dependencies lainnya
+        implementation("com.google.android.material:material:1.12.0")
         implementation("androidx.core:core-ktx:1.12.0")
         implementation("androidx.appcompat:appcompat:1.6.1")
         implementation("com.google.android.material:material:1.11.0")
